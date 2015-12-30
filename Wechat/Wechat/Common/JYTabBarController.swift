cff0cd5568fffaff655cd50323955b71d6693ddf
jiangzhibin <jasonryan@MrJiangdeMacBook-Pro.local>
2015-12-30 14:54:27 +0800

    pod 'SnapKit', '~> 0.18.0'

--- Wechat/Wechat/Common/JYTabBarController.swift
+++ Wechat/Wechat/Common/JYTabBarController.swift
@@ -9,5 +9,13 @@
 import UIKit
 
 class JYTabBarController: UITabBarController {
-    
+    private var tabBarBGView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBar.hidden = true
+        
+        self.tabBarBGView = UIImageView()
+        self.tabBarBGView.image = UIImage(named: "tabbarBkg")
+        
+    }
 }
